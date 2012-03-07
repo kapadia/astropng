2a74f58987a398b1ba76ce500cab91b24b5cfb9a
Amit Kapadia <amit@zooniverse.org>
2012-03-07 11:42:42 -0600

    create package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='astropng',
+    version='0.0.1',
+    description='Python package to convert FITS images to PNGs',
+    author='Amit Kapadia',
+    author_email='amit@zooniverse.org',
+    packages=['astropng']
+)
